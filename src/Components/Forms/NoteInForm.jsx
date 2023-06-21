72d2dcef1f37d2deaa8129b738e4a52b87c241a1
Your Name <hoangtan250997@gmail.com>
2023-06-21 23:14:48 +0700

    test note - in

--- src/Components/Forms/NoteInForm.jsx
+++ src/Components/Forms/NoteInForm.jsx
@@ -31,48 +31,6 @@ import { auto } from "@popperjs/core";
 
 // import Icon from "@ant-design/icons/lib/components/AntdIcon";
 
-// const SignupForm = () => {
-//   const formik = useFormik({
-//     initialValues: {
-//       supplierCode: "",
-//       productCode: "",
-//       amount: "",
-//       cost: "",
-//       areaCost: "",
-//       note: "",
-//     },
-//     validationSchema: Yup.object({
-//       supplierCode: Yup.string(),
-//       // .required("Required")
-//       // .min(4, "Must be 4 characters or more"),
-//       email: Yup.string()
-//         .required("Required")
-//         .matches(
-//           /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-//           "Please enter a valid email address"
-//         ),
-//       password: Yup.string()
-//         .required("Required")
-//         .matches(
-//           /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,19}$/,
-//           "Password must be 7-19 characters and contain at least one letter, one number and a special character"
-//         ),
-//       confirmedPassword: Yup.string()
-//         .required("Required")
-//         .oneOf([Yup.ref("password"), null], "Password must match"),
-//       phone: Yup.string()
-//         .required("Required")
-//         .matches(
-//           /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
-//           "Must be a valid phone number"
-//         ),
-//     }),
-//     onSubmit: (values) => {
-//       window.alert("Successful!!!");
-//       console.log(values);
-//     },
-//   });
-
 const useStyles = makeStyles({
   root: {
     background: "rgb(220,200,200)",
@@ -83,79 +41,74 @@ const useStyles = makeStyles({
 });
 
 function NoteInForm() {
-  // Điều chỉnh thư viên Material UI
   // Khởi tạo giá trị cho form
+
+  //1
   const classes = useStyles();
-  const [inputFields, setinputFields] = useState({
+  const [goodsreceivednotes, setgoodsreceivednotes] = useState({
     supplierCode: "",
     note: "",
-    inputField: [
-      {
-        detailId: uuidv4(),
-        productCode: "",
-        amount: "",
-        cost: "",
-        area: "",
-      },
-    ],
+    // incomingDetailsCreateDTOList: [],
   });
 
+  //2
+  //+-
+  const [incomingDetailsCreateDTOList, setincomingDetailsCreateDTOList] =
+    useState([
+      { id: uuidv4(), productCode: "", amount: "", cost: "", areaCode: "" },
+    ]);
+
+  //1
+  //Set giá trị cho form
   const handleChange = (event) => {
     const { name, value } = event.target;
-    // alert(value);
 
-    setinputFields((prevFormData) => ({ ...prevFormData, [name]: value }));
+    setgoodsreceivednotes((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   };
 
-  const handleSubmitConsole = (event) => {
-    event.preventDefault();
-    alert(
-      `SupplierCode: ${inputFields.supplierCode}, Note: ${inputFields.note},Production Code: ${inputFields.inputField[0].productCode}`
+  //2
+  //+-
+  const handleChangeInput = (id, event) => {
+    const newincomingDetailsCreateDTOList = incomingDetailsCreateDTOList.map(
+      (i) => {
+        if (id === i.id) {
+          i[event.target.name] = event.target.value;
+        }
+
+        return i;
+      }
     );
-
-    console.log("InputFields", inputFields);
+    setincomingDetailsCreateDTOList(newincomingDetailsCreateDTOList);
+    setgoodsreceivednotes((prevState) => ({
+      ...prevState,
+      incomingDetailsCreateDTOList: [...incomingDetailsCreateDTOList],
+    }));
   };
 
-  // Cho phép ghi giá trị vào ô và lấy giá trị
-  // const handleChangeInput = (event) => {
-  //   console.log("HERE");
-  //   const { name, value } = event.target;
-  //   console.log("name", name);
-  //   console.log("value", value);
-
-  //   setInputFields((prevFormData) => ({ ...prevFormData, [name]: value }));
-  // };
-
+  //1+2
   // Tạo ra object cho form
+  const merge = (event) => {
+    const updatedGoodsReceivedNotes = {
+      ...goodsreceivednotes,
+      incomingDetailsCreateDTOList: incomingDetailsCreateDTOList,
+    };
+    setgoodsreceivednotes(updatedGoodsReceivedNotes);
+    console.log("goodsreceivednotes", goodsreceivednotes);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("InputFields", inputFields);
+    // console.log("incomingDetailsCreateDTOList", incomingDetailsCreateDTOList);
+    console.log("goodsreceivednotes", goodsreceivednotes);
   };
-  //----------------------------------------------------
-  // const newInputFields = inputFields.map((i) => {
-  // console.log("ID2 là: ", i.id);
-  //   i[event.target.name] = event.target.value;
-  //   console.log("Giá trị ", event.target.name, event.target.value);
 
-  //   return i;
-  // });
-
-  // setInputFields(newInputFields);
-
-  // Đổi màu nền của Input
-  // const [inputValue, setInputValue] = useState("");
-  // const [isFilled, setIsFilled] = useState(false);
-
-  // const handleInputChange = (e) => {
-  //   console.log("HANDLE INPUT CHANGE");
-  //   const value = e.target.value;
-  //   setInputValue(value);
-  //   setIsFilled(value !== "");
-  // };
-  //----------------------------------------------------
-
+  //2
   //Option của thẻ Product code & Area
+
   const [productCode, setproductCode] = React.useState("");
   const [area, setArea] = React.useState("");
 
@@ -169,22 +122,22 @@ function NoteInForm() {
 
   // Dấu +
 
-  // const handleAddFields = () => {
-  //   setinputFields([
-  //     ...inputFields,
-  //     { id: uuidv4(), amount: "", cost: "", areaCode: "" },
-  //   ]);
-  // };
+  const handleAddFields = () => {
+    setincomingDetailsCreateDTOList([
+      ...incomingDetailsCreateDTOList,
+      { id: uuidv4(), amount: "", cost: "", areaCode: "" },
+    ]);
+  };
   // Dấu -
 
-  // const handleRemoveFields = (id) => {
-  //   const values = [...inputFields];
-  //   values.splice(
-  //     values.findIndex((value) => value.id === id),
-  //     1
-  //   );
-  //   setinputFields(values);
-  // };
+  const handleRemoveFields = (id) => {
+    const values = [...incomingDetailsCreateDTOList];
+    values.splice(
+      values.findIndex((value) => value.id === id),
+      1
+    );
+    setincomingDetailsCreateDTOList(values);
+  };
 
   return (
     <Container className={classes.root} style={{ height: "auto" }}>
@@ -197,7 +150,7 @@ function NoteInForm() {
       >
         IN NOTE FORM
       </Typography>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={merge}>
         {/* Nút Supplier Code */}
 
         <Box
@@ -215,12 +168,12 @@ function NoteInForm() {
             labelId="supplier-select-label"
             name="supplierCode"
             label="Supplier Code"
-            value={inputFields.supplierCode}
+            value={goodsreceivednotes.supplierCode}
             onChange={handleChange}
           >
-            <MenuItem value={10}>SHRI</MenuItem>
-            <MenuItem value={20}>SDE</MenuItem>
-            <MenuItem value={30}>TAN</MenuItem>
+            <MenuItem value="SHRI">SHRI</MenuItem>
+            <MenuItem value="SDE">SDE</MenuItem>
+            <MenuItem value="TAN">TAN</MenuItem>
           </Select>
 
           {/* Nút Note */}
@@ -231,7 +184,7 @@ function NoteInForm() {
             // label="Note (Optional)"
             name="note"
             variant="outlined"
-            value={inputFields.note}
+            value={goodsreceivednotes.note}
             // onChange={(event) => handleChangeInput(inputFields[0].id, event)}
             onChange={handleChange}
           />
@@ -249,91 +202,100 @@ function NoteInForm() {
             // columnGap: 3,
           }}
         >
-          {/* {inputFields.map((field, index) => (
-            <div key={index}> */}
-          {/* Production Code */}
-          <FormControl sx={{ marginRight: 1, minWidth: 250 }}>
-            <InputLabel style={{ margin: "0" }}>Product Code</InputLabel>
-            <Select
-              labelId="productCode"
-              id="productCode"
-              name="productCode"
-              value={inputFields.inputField[0].productCode}
-              label="Product Code"
-              // onChange={handleChange}
-              // onChange={(event) => {
-              //   changeProductCode(event);
-              // }}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={1}>W180</MenuItem>
-              <MenuItem value={2}>W210</MenuItem>
-              <MenuItem value={3}>W240</MenuItem>
-            </Select>
-          </FormControl>{" "}
-          {/* Amount */}
-          {/* <TextField
-            id="amount"
-            style={{ marginRight: "10px", minWidth: "250px" }}
-            name="amount"
-            label="Amount (kg)"
-            value={inputFields[0].incomingDetails[0].amount}
-            // onChange={(event) => {
-            //   handleChangeInput(event.target.id, event);
-            // handleInputChange(event);
+          {incomingDetailsCreateDTOList.map((incomingDetailsCreateDTO) => (
+            <div key={incomingDetailsCreateDTO.id}>
+              {/* Production Code */}
+              <FormControl sx={{ marginRight: 1, minWidth: 250 }}>
+                <InputLabel style={{ margin: "0" }}>Product Code</InputLabel>
+                <Select
+                  labelId="productCode"
+                  id="productCode"
+                  name="productCode"
+                  value={incomingDetailsCreateDTO.productCode}
+                  label="Product Code"
+                  // onChange={handleChange}
+                  // onChange={(event) => {
+                  //   changeProductCode(event);
+                  // }}
+                  onChange={(event) =>
+                    handleChangeInput(incomingDetailsCreateDTO.id, event)
+                  }
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="W180">W180</MenuItem>
+                  <MenuItem value="W210">W210</MenuItem>
+                  <MenuItem value="W240">W240</MenuItem>
+                </Select>
+              </FormControl>{" "}
+              {/* Amount  */}
+              <TextField
+                id="amount"
+                style={{ marginRight: "10px", minWidth: "250px" }}
+                name="amount"
+                label="Amount (kg)"
+                value={incomingDetailsCreateDTO.amount}
+                // onChange={(event) => {
+                //   handleChangeInput(event.target.id, event);
+                // handleInputChange(event);
 
-            // }}
-            onChange={handleChange}
+                // }}
+                onChange={(event) =>
+                  handleChangeInput(incomingDetailsCreateDTO.id, event)
+                }
 
-            // className={isFilled ? "filled" : "inNotesNode"}
-          /> */}
-          {/* Cost */}
-          {/* <TextField
-            style={{ marginRight: "10px", minWidth: "250px" }}
-            name="cost"
-            label="Cost"
-            value={inputFields[0].incomingDetails[0].cost}
-            // onChange={(event) => {
-            //   handleChangeInput(inputField.id, event);
-            //   // handleInputChange(event);
-            // }}
-            onChange={handleChange}
+                // className={isFilled ? "filled" : "inNotesNode"}
+              />
+              {/* Cost */}
+              <TextField
+                style={{ marginRight: "10px", minWidth: "250px" }}
+                name="cost"
+                label="Cost"
+                value={incomingDetailsCreateDTO.cost}
+                // onChange={(event) => {
+                //   handleChangeInput(inputField.id, event);
+                //   // handleInputChange(event);
+                // }}
+                onChange={(event) =>
+                  handleChangeInput(incomingDetailsCreateDTO.id, event)
+                }
 
-            // className={isFilled ? "filled" : "inNotesNode"}
-          ></TextField> */}
-          {/* Area */}
-          {/* <FormControl sx={{ marginRight: 1, minWidth: 250 }}>
-            <InputLabel style={{ margin: "0" }}>Area</InputLabel>
-            <Select
-              labelId="areaCode"
-              id="areaCode"
-              value={inputFields[0].incomingDetails[0].area}
-              label="Area Code"
-              onChange={changeArea}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>W180</MenuItem>
-              <MenuItem value={20}>W210</MenuItem>
-              <MenuItem value={30}>W240</MenuItem>
-            </Select>
-          </FormControl> */}
-          {/* Button*/}
-          {/* <IconButton
-            disabled={inputFields.length === 1}
-            // onClick={() => handleRemoveFields(inputField.id)}
-          >
-            <RemoveIcon />
-          </IconButton>
-
-          <IconButton onClick={handleAddFields}>
-            <AddIcon />
-          </IconButton> */}
-          {/* </div> */}
-          {/* ))} */}
+                // className={isFilled ? "filled" : "inNotesNode"}
+              ></TextField>
+              {/* Area */}
+              <FormControl sx={{ marginRight: 1, minWidth: 250 }}>
+                <InputLabel style={{ margin: "0" }}>Area</InputLabel>
+                <Select
+                  labelId="areaCode"
+                  name="areaCode"
+                  id="areaCode"
+                  value={incomingDetailsCreateDTO.area}
+                  label="Area Code"
+                  onChange={(event) =>
+                    handleChangeInput(incomingDetailsCreateDTO.id, event)
+                  }
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="W18">W18</MenuItem>
+                  <MenuItem value="W21">W21</MenuItem>
+                  <MenuItem value="W24">W24</MenuItem>
+                </Select>
+              </FormControl>
+              {/* Button */}
+              <IconButton
+                disabled={incomingDetailsCreateDTOList.length === 1}
+                onClick={() => handleRemoveFields(incomingDetailsCreateDTO.id)}
+              >
+                <RemoveIcon />
+              </IconButton>
+              <IconButton onClick={handleAddFields}>
+                <AddIcon />
+              </IconButton>
+            </div>
+          ))}
         </Box>
 
         {/* ----------------------------------------------------------------- */}
@@ -348,7 +310,7 @@ function NoteInForm() {
             variant="contained"
             color="primary"
             type="submit"
-            onClick={handleSubmitConsole}
+            onClick={handleSubmit}
           >
             Submit
           </Button>
@@ -357,5 +319,4 @@ function NoteInForm() {
     </Container>
   );
 }
-
 export default NoteInForm;
